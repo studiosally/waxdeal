e543c02f345d4cf578f6880ee826c4815798d438
Wally Torres <wally@studiosally.com>
2017-06-01 13:43:15 +0200

    fixing login button to profile

--- public/js/main.js
+++ public/js/main.js
@@ -194,5 +194,12 @@
 		loaderPage();
 	});
 
+$('input,textarea').focus(function(){
+   $(this).data('placeholder',$(this).attr('placeholder'))
+   $(this).attr('placeholder','');
+});
+$('input,textarea').blur(function(){
+   $(this).attr('placeholder',$(this).data('placeholder'));
+});
 
 }());
